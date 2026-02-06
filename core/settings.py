3df47898c25fea77362e499caee595649e172065
Aneeqa Baig <aneeqabaig@sihpp.gos.pk>
2026-02-06 17:41:59 +0500

    settings

--- core/settings.py
+++ core/settings.py
@@ -115,8 +115,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    "*",
+]
